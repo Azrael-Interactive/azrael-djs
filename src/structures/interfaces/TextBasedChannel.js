4c448a824bb0b05b12b81259354bf0612af3690a
Airbus A330-200 <53490656+VietnamAirlines-lab@users.noreply.github.com>
2023-03-30 18:10:57 -0700

    Program message send

--- src/structures/interfaces/TextBasedChannel.js
+++ src/structures/interfaces/TextBasedChannel.js
@@ -156,16 +156,31 @@ class TextBasedChannel {
    *   .then(console.log)
    *   .catch(console.error);
    */
-  async send(options) {
+  async send(content, options) {
     const User = require('../User');
     const { GuildMember } = require('../GuildMember');
+
+    let messagePayload;
+
+    if (!options) options = {};
+    if (typeof content == "string") {
+        options.content = content
+    } else if (typeof content == "object" && content?.type == "rich") {
+        options.embeds = [content]
+    } else if (typeof content == "object" && typeof content?.embed == "object") {
+        options = content
+        options.embeds = [content?.embed]
+    } else {
+        options = content
+        if (options?.embed) {
+          options.embeds = [options.embed]
+        }
+    }
 
     if (this instanceof User || this instanceof GuildMember) {
       const dm = await this.createDM();
       return dm.send(options);
     }
-
-    let messagePayload;
 
     if (options instanceof MessagePayload) {
       messagePayload = options.resolveData();
